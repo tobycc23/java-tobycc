a158d8c2e4beaee28df5a11e042d5d36209ef42c
Toby Christey-Clove <toby.christey-clover@algomi.com>
2018-01-26 15:06:42 +0000

    Further Collections and Lambda and Aggregate Operations learning

--- src/main/java/com/tobycc/java/learning/AggregateOpsLearning.java
+++ src/main/java/com/tobycc/java/learning/AggregateOpsLearning.java
@@ -1,26 +1,99 @@
 package com.tobycc.java.learning;
 
 import com.tobycc.java.model.Person;
+import com.tobycc.java.model.enums.Gender;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by toby.christey-clover on 25/01/2018.
  */
 public class AggregateOpsLearning {
 
-    public static void main(String[] args) {
-        //Using the example from LambdaLearning.java we can match the functional interfaces to aggregate operations
+    private static final List<Person> list = new ArrayList<>(Arrays.asList(Person.randomListOfPeople()));
 
+    //1. comparing traditional way to manipulate Collection and Java 8 way of using aggregate functions
+    public static void collectionVsAggregateOps() {
         //Obtain a source of objects, filter objects that match a Predicate object, map objects to another value as
         //specified by a Function object, then perform an action as specified by a Consumer object
-        List<Person> list = new ArrayList<>();
         list.stream()  //Stream<E> stream()
                 .filter(p -> p.getAge() >= 50) 	//Stream<T> filter(Predicate<? super T> predicate)
                 .map(p -> p.getEmailAddress())  //<R> Stream<R> map(Function<? super T, ? extends R> mapper)
                 .forEach(email -> System.out.println(email)); //void forEach(Consumer<? super T> action)
 
-        //Unlike a collection, this is a pipeline of ops
+        //Unlike a collection, this is a pipeline of ops with a stream (sequence of elements) running through it
+
+        //The above can be translated into the traditional way:
+        for(Person p : list) {
+            if(p.getAge() >= 50) {
+                String email = p.getEmailAddress();
+                System.out.println(email);
+            }
+        }
+    }
+
+
+    //2. Using aggregate operations further and reduction ops
+    public static void furtherAggregateops() {
+        double average;
+        average = list.stream()
+                .filter(p -> p.getGender() == Gender.MALE)
+                .mapToInt(Person::getAge) //or use .mapToInt(p -> p.getAge()) ...same thing
+                .average()
+                .getAsDouble();
+        //JDK contains many terminal operations (such as average, sum, min, max, and count) that return one value by
+        //combining the contents of a stream; reduction ops.
+        System.out.println(average);
+    }
+
+
+    //3. Reduction examples
+    public static void reduceExample() {
+        //sum() is a reduction operation
+        Integer totalAge;
+        totalAge = list.stream()
+                .mapToInt(Person::getAge)
+                .sum();
+        System.out.println(totalAge);
+
+        //Stream.reduce() is a general-purpose reduction op which takes in the Identity (init value / default if no
+        //elements in stream) and Accumulator (takes parameters which are partial result of reduction so far and next
+        //element, returning a new partial result)
+        Integer totalAgeReduce;
+        totalAgeReduce = list.stream()
+                .map(Person::getAge)
+                .reduce(0, (a, b) -> a + b);
+                //.reduce(0, AggregateOpsLearning::aggSum);  //could use this line instead of the one above
+        System.out.println(totalAgeReduce);
+    }
+
+    public static int aggSum(int a, int b) {
+        return a+b;
+    }
+
+
+    //4. If your reduce op involves adding elements to a collection, note every time your accumulator processes
+    //an element it creates a new collection to include the element, highly inefficient. With Stream.collect you update
+    //existing collection instead.
+    public static void collectExample() {
+        Collection<String> emailAddressesOverFifties;
+        emailAddressesOverFifties = list.stream()  //Stream<E> stream()
+                .filter(p -> p.getAge() >= 50)
+                .map(p -> p.getEmailAddress())
+                .collect(Collectors.toList());
+        System.out.println(emailAddressesOverFifties);
+    }
+
+
+    public static void main(String[] args) {
+        //Using the example from LambdaLearning.java we can match the functional interfaces to aggregate operations
+        collectionVsAggregateOps();
+        furtherAggregateops();
+        reduceExample();
+        collectExample();
     }
 }
--- src/main/java/com/tobycc/java/model/Person.java
+++ src/main/java/com/tobycc/java/model/Person.java
@@ -14,19 +14,56 @@ public class Person {
     private String emailAddress;
     private int age;
 
+    public Person() {
+//..
+    }
+
+    public Person(String name, LocalDate birthday, Gender gender, String emailAddress, int age) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+        this.age = age;
+    }
+
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) { this.age = age; }
 
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
     public String getEmailAddress() { return emailAddress; }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 
     public void printPerson() {
         System.out.println(this.toString());
     }
 
+
     public static int compareAges(Person a, Person b) {
         return a.getAge()>b.getAge() ? 1 : 0;
+    }
+
+    public static Person[] randomListOfPeople() {
+        Person a1 = new Person("a1", null, Gender.MALE, "a1@gmail.com", 55);
+        Person a2 = new Person("a2", null, Gender.MALE, "a2@gmail.com", 24);
+        Person a3 = new Person("a3", null, Gender.MALE, "a3@gmail.com", 33);
+        Person b1 = new Person("b1", null, Gender.FEMALE, "b1@gmail.com", 21);
+        Person b2 = new Person("b2", null, Gender.FEMALE, "b2@gmail.com", 49);
+        Person b3 = new Person("b3", null, Gender.FEMALE, "b3@gmail.com", 80);
+        Person b4 = new Person("b4", null, Gender.FEMALE, "b4@gmail.com", 34);
+
+        return new Person[]{a1,a2,a3,b1,b2,b3,b4};
     }
 }
