aed8e584ae8961802e58b2574a0c3dd59101b290
toby.christey-clover <toby.christey-clover@gmail.com>
2018-04-30 11:48:43 +0100

    Webservice with SpringBoot learning

---
+++ src/main/java/com/tobycc/java/learning/webservice/Greeting.java
@@ -0,0 +1,23 @@
+package com.tobycc.java.learning.webservice;
+
+/**
+ * Created by toby.christey-clover on 23/04/2018.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/com/tobycc/java/learning/webservice/GreetingController.java
@@ -0,0 +1,42 @@
+package com.tobycc.java.learning.webservice;
+
+/**
+ * Created by toby.christey-clover on 23/04/2018.
+
+ @RequestParam binds the value of the query string parameter name into the name parameter of the greeting() method. If
+ the name parameter is absent in the request, the defaultValue of "World" is used.
+
+ The implementation of the method body creates and returns a new Greeting object with id and content attributes based on
+ the next value from the counter, and formats the given name by using the greeting template.
+
+ A key difference between a traditional MVC controller and the RESTful web service controller above is the way that the
+ HTTP response body is created. Rather than relying on a view technology to perform server-side rendering of the greeting
+ data to HTML, this RESTful web service controller simply populates and returns a Greeting object. The object data will
+ be written directly to the HTTP response as JSON.
+
+ This code uses Spring 4’s new @RestController annotation, which marks the class as a controller where every method
+ returns a domain object instead of a view. It’s shorthand for @Controller and @ResponseBody rolled together.
+
+ The Greeting object must be converted to JSON. Thanks to Spring’s HTTP message converter support, you don’t need to do
+ this conversion manually. Because Jackson 2 is on the classpath, Spring’s MappingJackson2HttpMessageConverter is
+ automatically chosen to convert the Greeting instance to JSON.
+
+
+ */
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
